eed4ac8d877c730996a1959f9584a942b6b2f6f7
mengmeng19 <mengmeng19@staff.weibo.com>
2020-04-05 22:59:22 +0800

    webpack plugin html-webpack-plugin

---
+++ src/02.webpack-打包html资源/webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.[hash].js',
+  },
+  module: {
+    rules: [
+      {
+        test: /.\css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.less$/,
+        use: ['style-loader', 'css-loader', 'less-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html', // 指定模版文件。复制指定template对应的index.html并自动引入所有打包后的资源(js/css)
+      filename: 'test.html', // 指定打包后文件名
+      // hash: true, // 为了避免缓存 在产出的文件后面可以加上hash
+      // chunks: ['index']
+    })
+  ],
+}
